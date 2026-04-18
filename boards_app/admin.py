3b2768ab2fe42ce3ae5bc21d0d2074ed03daa8e1
Felix R <nikojin@gmx.net>
2026-04-18 11:33:57 +0200

    add admin.py  permissions

--- boards_app/admin.py
+++ boards_app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    fieldsets = UserAdmin.fieldsets + (
+        ("Custom Fields", {"fields": ("fullname",)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Custom Fields", {
+            "classes": ("wide",),
+            "fields": ("email", "username", "fullname", "password1", "password2"),
+        }),
+    )
+
+    list_display = ("email", "username", "fullname", "is_staff", "is_superuser", "is_active")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+
+    search_fields = ("email", "username", "fullname")
+    ordering = ("email",)
+
+    readonly_fields = ()
