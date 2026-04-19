b21b189487f0f41a363166d26ff1affa8d4f921f
Felix R <nikojin@gmx.net>
2026-04-19 11:01:12 +0200

    add board_app permissions

---
+++ boards_app/api/permissions.py
@@ -0,0 +1,23 @@
+from rest_framework.permissions import BasePermission
+
+class IsBoardOwnerOrMemberOrSuperuser(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+
+        if user.is_superuser:
+            return True
+
+        if request.method == "DELETE":
+            return obj.owner_id == user.id
+
+        if request.method in ["GET", "PATCH"]:
+            return (
+                obj.owner_id == user.id
+                or obj.members.filter(id=user.id).exists()
+            )
+
+        return False
